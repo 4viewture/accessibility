075d8101a27b9264198b34baa274e773a87ace9f
Kay Strobach <info@kay-strobach.de>
2026-02-12 14:45:29 +0100

    [TASK] extract page.tsconfig for v14
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,25 +2,6 @@
 
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-// Add content element to the "New Content Element" wizard
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    'mod {
-        wizards.newContentElement.wizardItems.special {
-            elements {
-                accessibility_barrier_free_content {
-                    iconIdentifier = tx-accessibility-barrier-free-content
-                    title = LLL:EXT:accessibility/Resources/Private/Language/locallang_db.xlf:tt_content.tx_accessibility_barrier_free.title
-                    description = LLL:EXT:accessibility/Resources/Private/Language/locallang_db.xlf:tt_content.tx_accessibility_barrier_free.description
-                    tt_content_defValues {
-                        CType = accessibility_barrier_free_content
-                    }
-                }
-            }
-            show := addToList(accessibility_barrier_free_content)
-        }
-    }'
-);
-
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1751463991] = [
     'nodeName' => 'accessibility_helptext',
     'priority' => 40,
